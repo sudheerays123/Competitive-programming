263ee3cfe74730a84d84dbeacc79905971b7d23d
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-06-25 18:15:22 +0530

    Updating with AC Code

--- SPOJ/Graphs/BFS/Kaththi.cpp
+++ SPOJ/Graphs/BFS/Kaththi.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define ll int
-#define tc ll test;cin>>test;while(test--)
+#define ll long long int
+#define tc ll test;cin >> test;while(test--)
 #define vi vector<ll>
 #define pll pair<ll,ll>
 #define pb push_back
 #define mp make_pair
-#define INF 10000
+#define INF 1e18
 #define MOD 1e9 + 7
 #define ff first
 #define ss second
@@ -30,25 +30,27 @@ vector<vector<char>> grid(N,vector<char>(N));
 
 ll bfs(){
 
-    queue<pll> q;
+    deque<pll> q;
     vector<vi> gkill(n+5,vi(m+5,INF));
 
-    q.push({1,1});
+    q.push_front({1,1});
     gkill[1][1] = 0;
 
     while(!q.empty()){
         ll a = q.front().first;
         ll b = q.front().second;
 
-        q.pop();
+        q.pop_front();
 
         fo(i,0,3){
             ll aa = a + dx[i];
             ll bb = b + dy[i];
 
-            if(aa >= 1 && aa <= n && bb >= 1 && bb <=m && gkill[aa][bb] > gkill[a][b]){
+            if(aa >= 1 && aa <= n && bb >= 1 && bb <=m && gkill[aa][bb] > gkill[a][b] + (grid[aa][bb] != grid[a][b])){
                 gkill[aa][bb] = gkill[a][b] + (grid[aa][bb] != grid[a][b]);
-                q.push(mp(aa,bb));
+
+                if(grid[aa][bb] == grid[a][b]) q.push_front(mp(aa,bb));
+                if(grid[aa][bb] != grid[a][b]) q.push_back(mp(aa,bb));
             }
         }
     }
